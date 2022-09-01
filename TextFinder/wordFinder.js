71025065d8f17cd9b8500c5b64981c387749de9a
GuessMF <segas95@yandex.ru>
2022-09-01 14:39:17 +0300

    main

--- TextFinder/wordFinder.js
+++ TextFinder/wordFinder.js
@@ -1,42 +1,29 @@
-let bodyy = document.body.innerHTML;
-let stat = false;
-let foundButton = document.getElementById("found");
+const DIV = content.innerHTML;
+stat = false;
 
-// стата Фолс, после поиска буквы стата Тру, жмем на инпут, проверяется на стату, если она Тру, меняем на изначальную страницу
-function newPage() {
-  document.body.innerHTML = bodyy;
-}
-
-foundButton.addEventListener("click", () => {
+newPage = () => {
+  content.innerHTML = DIV;
+  changeMe.innerHTML = " ";
+};
+found.addEventListener("click", () => {
   newCheck();
-  console.log(stat);
 });
 
 input.addEventListener("click", () => {
-  if (stat == true) {
-    newPage();
-  }
-  console.log(stat);
+  stat == true ? newPage() : console.log("no");
 });
 
 function newCheck() {
-  let letterToFind = document.getElementById("input").value; //буква введенная в инпут
-
-  let massives = []; //добавление всех спанов в массив
-  let spansLength = document.getElementsByTagName("span").length; // колличество спанов в документе
-  let body, mass;
-  let words = []; //для отслеживания колличества изменений
-  let changed = document.getElementById("changeMe");
-
+  massives = [];
+  spansLength = document.getElementsByTagName("span").length; // колличество спанов в документе
+  words = []; //для отслеживания колличества изменений
   for (i = 1; i < spansLength + 1; i++) {
     body = document.getElementById(i); //все имеющиеся спаны (в скобках айди)
     mass = body.innerHTML.split(""); // посимвольное добавление в массив каждого спана
     massives.push(mass);
-
     for (i = 0; i < massives.length; i++) {
       for (n = 0; n < massives[i].length; n++) {
-        if (massives[i][n] == letterToFind) {
-          //поверка совпадения заглавной буквы из массива с введенной заглавной
+        if (massives[i][n] == input.value) {
           massives[i][n] = "<font color=red>" + massives[i][n] + "</font>"; //добавление цвета
           body.innerHTML = mass.join(""); // новая html с измененным цветом
           words.push(massives[i][n]); // добавление в массив измененных символов
@@ -44,25 +31,7 @@ function newCheck() {
       }
     }
   }
-
-  changed.innerHTML = words.length;
+  changeMe.innerHTML = words.length;
   stat = true;
-
   input.value = "";
 }
-
-// function innnn() {
-//   if (clk == true && document.body.innerHTML !== bodyy) {
-//     //newPage();
-//     clk = false;
-//     console.log("nado obnovit");
-//   }
-// }
-
-// function testing() {
-//   newCheck();
-//   if (document.body.innerHTML !== bodyy) {
-//
-//     // newCheck();
-//   }
-// }

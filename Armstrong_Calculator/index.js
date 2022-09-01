e72d43a31e53a102bb5648bbe994c434094e23a3
GuessMF <segas95@yandex.ru>
2022-09-01 18:16:02 +0300

    main

--- Armstrong_Calculator/index.js
+++ Armstrong_Calculator/index.js
@@ -3,6 +3,8 @@ let armstrongPloshad = 0.36; //sm
 
 //расчитывает  площадь комнаты и рисует ее
 //нужно чтобы считал и раставлял блоки относительно ширины комнаты и рисовал это поверх комнаты
+
+let div;
 calculate.addEventListener("click", () => {
   ploshad = width.value * height.value; //площадь комнаты
   rezultat = ploshad / armstrongPloshad; // количество плит
@@ -10,16 +12,30 @@ calculate.addEventListener("click", () => {
   console.log("blocks" + rezultat);
   console.log("ploshad" + ploshad);
 
-  drawMaze(height.value, width.value, div);
+  drawRoom(width.value, height.value);
+  drawBoxs(height.value, width.value, div);
 });
 
-let div = document.getElementById("container");
+//let div = document.getElementById("div");
 
-function drawMaze(col, row, parent) {
+function drawRoom(width, height) {
+  div = document.createElement("div");
+  div.className = "test";
+  div.style.cssText = `
+  width: ${width * 100}px;
+  height: ${height * 100}px
+`;
+
+  container.appendChild(div);
+  drawBoxs(height.value, width.value, div);
+}
+
+function drawBoxs(col, row, parent) {
   let table = document.createElement("table");
   table.className = "table";
   for (i = 0; i < row; i++) {
     let tr = document.createElement("tr");
+
     table.appendChild(tr);
 
     for (n = 0; n < col; n++) {

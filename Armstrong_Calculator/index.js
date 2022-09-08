e47ee99c827632f8db777a806d51db1873c9e982
GuessMF <segas95@yandex.ru>
2022-09-08 20:02:14 +0300

    ьфшт

--- Armstrong_Calculator/index.js
+++ Armstrong_Calculator/index.js
@@ -1,36 +1,51 @@
 //let armstrongPloshad = 0.36; //metr
 // сделать валидацию в инпуты чтоб вместо запятой была точка
 // поправить размытие при дробных числах 2 и 2,61
+//добавить блоки а б с для не отцентрованного лабиринта
+//внести в таблицу
+//отредактировать что бы при нуле не писал в таблицу и не выводил букву
+//сделать букву в канвасе четче
 
 let div;
 calculate.addEventListener("click", () => {
-  a = Number(width.value);
-  b = Number(height.value);
-  ploshadd.innerHTML = (a * b).toFixed(2) + " m²";
+  // a = Number(width.value);
+  // b = Number(height.value);
+  // ploshadd.innerHTML = (a * b).toFixed(2) + " m²";
 
-  perimetr.innerHTML = ((a + b) * 2).toFixed(2);
-  blocks.innerHTML = ((a * b) / 0.36).toFixed(1);
-  visote.innerHTML = (b / 0.6).toFixed(1);
-  shirine.innerHTML = (a / 0.6).toFixed(1);
+  // perimetr.innerHTML = ((a + b) * 2).toFixed(2);
+  // blocks.innerHTML = ((a * b) / 0.36).toFixed(1);
+  // visote.innerHTML = (b / 0.6).toFixed(1);
+  // shirine.innerHTML = (a / 0.6).toFixed(1);
 
-  ostPrav = Number(shirine.innerHTML) - Math.floor(a / 0.6);
-  ostatokPrav.innerHTML = (ostPrav * 0.6).toFixed(2);
+  // ostPrav = Number(shirine.innerHTML) - Math.floor(a / 0.6);
+  // ostatokPrav.innerHTML = (ostPrav * 0.6).toFixed(2);
 
-  ostNiz = Number(visote.innerHTML) - Math.floor(b / 0.6);
+  // ostNiz = Number(visote.innerHTML) - Math.floor(b / 0.6);
 
-  ostatokNiz.innerHTML = (ostNiz * 0.6).toFixed(2);
-  ostatokNiz.innerHTML == 0 || ostatokPrav.innerHTML == 0
-    ? console.log("")
-    : (melkyi.innerHTML =
-        " Выс  " +
-        ostatokNiz.innerHTML +
-        "  " +
-        " Шир  " +
-        ostatokPrav.innerHTML);
+  // ostatokNiz.innerHTML = (ostNiz * 0.6).toFixed(2);
+  // ostatokNiz.innerHTML == 0 || ostatokPrav.innerHTML == 0
+  //   ? console.log("")
+  //   : (melkyi.innerHTML =
+  //       " Выс  " +
+  //       ostatokNiz.innerHTML +
+  //       "  " +
+  //       " Шир  " +
+  //       ostatokPrav.innerHTML);
 
   canvasDraw(width.value, height.value);
 });
 centred.addEventListener("click", () => {
+  a = Number(width.value);
+  b = Number(height.value);
+
+  ploshad.innerHTML = (a * b).toFixed(2);
+  perimetr.innerHTML = ((a + b) * 2).toFixed(2);
+  blocks.innerHTML = ((a * b) / 0.36).toFixed(1);
+
+  razmA;
+  razmB;
+  razmC;
+
   canvasDrawCentred(width.value, height.value);
 });
 
@@ -92,7 +107,7 @@ function canvasDrawCentred(width, height) {
       ver = kraiHor + 0.75 + n * 60; // шаг между вертикальными линиями
       hor = kraiVert + 0.75 + i * 60; //шаг между горизонтальными линиями
       //firstHor = (clientWidth - hor) / 2;
-      console.log(hor);
+      //console.log(hor);
       ctx.fillRect(0, 0, clientWidth, clientHeight); //фон
       ctx.moveTo(hor, 0);
       ctx.lineTo(hor, clientHeight);
@@ -101,8 +116,23 @@ function canvasDrawCentred(width, height) {
       ctx.stroke();
     }
   }
-  console.log(kraiVert + "   kraivert");
-  console.log(kraiHor + "   kraiHor");
+  razmA.innerHTML =
+    `${(kraiHor / 100).toFixed(2)}` + " x " + `${(kraiVert / 100).toFixed(2)}`;
+  // esli 0, to ne pokaz
+  razmB.innerHTML = `${(kraiHor / 100).toFixed(2)}` + " x  0.6";
 
-  console.log(hor);
+  razmC.innerHTML = "0.6 x " + `${(kraiVert / 100).toFixed(2)}`;
+
+  console.log(kraiHor + " kraiHor "); // высота обрезков А
+  //console.log(kraiVert + " kraiVert " + " ");
+  // console.log(hor);
+
+  ctx.fillStyle = "#00F";
+  ctx.font = "8pt Arial";
+  // ctx.fillWidth = 1;
+  ctx.fillText("a", kraiVert / 2 - 5, kraiHor);
+  ctx.fillText("b", kraiVert + 25, kraiHor);
+  ctx.fillText("c", kraiVert / 2 - 5, kraiHor + 60);
+  // ctx.fillText(`${ostatokPrav.innerHTML}`, hor, ver - 2);
+  // сtx.strokeText(`${ostatokPrav.innerHTML}`, ver - 1, clientHeight - 30);
 }

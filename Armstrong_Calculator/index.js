56593c665c4c5c4910ca24bab45c96a993ffcf09
GuessMF <segas95@yandex.ru>
2022-09-08 13:38:27 +0300

    main

--- Armstrong_Calculator/index.js
+++ Armstrong_Calculator/index.js
@@ -12,8 +12,23 @@ calculate.addEventListener("click", () => {
 
   perimetr.innerHTML = (a + b) * 2;
   blocks.innerHTML = ((a * b) / 0.36).toFixed(1);
-  visote.innerHTML = (a / 0.6).toFixed(1);
-  shirine.innerHTML = (b / 0.6).toFixed(1);
+  visote.innerHTML = (b / 0.6).toFixed(1);
+  shirine.innerHTML = (a / 0.6).toFixed(1);
+
+  ostPrav = Number(shirine.innerHTML) - Math.floor(a / 0.6);
+  ostatokPrav.innerHTML = (ostPrav * 0.6).toFixed(2);
+
+  ostNiz = Number(visote.innerHTML) - Math.floor(b / 0.6);
+
+  ostatokNiz.innerHTML = (ostNiz * 0.6).toFixed(2);
+  ostatokNiz.innerHTML == 0 || ostatokPrav.innerHTML == 0
+    ? console.log("yes")
+    : (melkyi.innerHTML =
+        " Выс  " +
+        ostatokNiz.innerHTML +
+        "  " +
+        " Шир  " +
+        ostatokPrav.innerHTML);
 
   canvasDraw(width.value, height.value);
 });
@@ -30,22 +45,29 @@ function canvasDraw(width, height) {
   ctx.fillStyle = "rgb(246, 234, 220)";
   ctx.strokeStyle = "black";
   //без центровки
-  for (i = 0; i < Math.trunc(width / 0.6); i++) {
-    for (n = 0; n < Math.trunc(height / 0.6); n++) {
-      ver = 60 + 0.5 + n * 60; // шаг между вертикальными линиями
-      hor = 60 + 0.5 + i * 60; //шаг между горизонтальными линиями
-      ctx.fillRect(0, 0, clientWidth, clientHeight);
-      ctx.moveTo(hor, 0);
-      ctx.lineTo(hor, clientHeight);
-      ctx.moveTo(0, ver);
-      ctx.lineTo(clientWidth, ver);
-      ctx.stroke();
-    }
-  }
-  ctx.fillStyle = "#00F";
-  ctx.strokeStyle = "#F00";
-  ctx.font = "10pt Arial";
-  ctx.fillText("Fill text", 20.5, 50.5);
-  ctx.font = "bold 10px sans-serif";
-  ctx.strokeText("Stroke text", 20, 100);
+  // for (i = 0; i < Math.trunc(width / 0.6); i++) {
+  //   for (n = 0; n < Math.trunc(height / 0.6); n++) {
+  //     ver = 60 + 0.5 + n * 60; // шаг между вертикальными линиями
+  //     hor = 60 + 0.5 + i * 60; //шаг между горизонтальными линиями
+  //     ctx.fillRect(0, 0, clientWidth, clientHeight);
+  //     ctx.moveTo(hor, 0);
+  //     ctx.lineTo(hor, clientHeight);
+  //     ctx.moveTo(0, ver);
+  //     ctx.lineTo(clientWidth, ver);
+  //     ctx.stroke();
+  //   }
+  // }
+  // console.log(n);
+  // console.log(i);
+  // ctx.fillStyle = "#00F";
+  // ctx.strokeStyle = "#F00";
+  // ctx.font = "6pt Arial";
+  // ctx.textWidth = 0.1;
+  // ctx.fillText(`${ostatokNiz.innerHTML}`, ver + 1, clientHeight); // высота нижнего
+  // ctx.font = "10px";
+  // ctx.strokeText(`${ostatokPrav.innerHTML}`, ver - 1, clientHeight - 30);
+  // // ширина нижнего
+
+  ctx.font = "30px";
+  ctx.strokeText("hello", 50, 50);
 }
